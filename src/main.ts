ab3214910766eab7fd9bd1897f17326d34629a8e
Hosam Tarade <tardehosam@gmail.com>
2026-09-01 08:18:31 -0700

    feat: add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,18 @@ async function bootstrap() {
 
   const config = app.get(ConfigService);
   const port = config.get<number>('port') ?? 3000;
+
+    const swaggerConfig = new DocumentBuilder()
+    .setTitle('Job & Internship Platform API')
+    .setDescription(
+      'REST API for companies to post jobs and candidates to apply',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(port);
   console.log(`Application running on http://localhost:${port}/api`);
