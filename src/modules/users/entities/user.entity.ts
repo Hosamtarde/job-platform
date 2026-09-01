edc9f4c81d84ac76eb87b60c3d47f18153bf5e32
Hosam Tarade <tardehosam@gmail.com>
2026-09-01 05:28:12 -0700

    feat: add user entity and create users table migration

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,38 @@
+﻿import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserRole } from '../../../common/enums';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  fullName: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+  })
+  role: UserRole;
+
+  @Column({ type: 'varchar', nullable: true, select: false })
+  hashedRefreshToken: string | null;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
