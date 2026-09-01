8433e24b83d042d1c3ac74f01dd27a1a86e74dc1
Hosam Tarade <tardehosam@gmail.com>
2026-09-01 08:02:58 -0700

    feat: add jwt strategies, guards and custom decorators

---
+++ src/modules/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,32 @@
+﻿import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { JwtPayload } from './jwt.strategy';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('jwt.refreshSecret') ?? '',
+      passReqToCallback: true,
+    });
+  }
+
+  validate(req: Request, payload: JwtPayload) {
+    const authHeader = req.get('authorization');
+    const refreshToken = authHeader?.replace('Bearer', '').trim();
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token malformed');
+    }
+
+    return { ...payload, refreshToken };
+  }
+}
